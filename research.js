41bc0aba6debddfe31492cb2b81ca18b0ede1be0
Valentin Kestelman <valaourok@gmail.com>
2021-09-01 10:10:36 +0200

    inventaires affichés en majuscules - algo index

--- research.js
+++ research.js
@@ -118,7 +118,7 @@ function getIngredientArray(recipe, normalizeAccents) {
     if (normalizeAccents) {
         return recipe.ingredients.map(item => item.ingredient.toLocaleLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, ''));
     }else{
-        return recipe.ingredients.map(item => item.ingredient.toLocaleLowerCase());
+        return recipe.ingredients.map(item => item.ingredient);
     }
 }
 
@@ -130,7 +130,7 @@ function applianceInventory(recipes, normalizeAccents) {
     if (normalizeAccents) {
         return new Set (recipes.map(recipe => recipe.appliance.toLocaleLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, '')));
     }else{
-        return new Set (recipes.map(recipe => recipe.appliance.toLocaleLowerCase()));
+        return new Set (recipes.map(recipe => recipe.appliance));
     }
 }
 
@@ -141,7 +141,7 @@ function ustensilsInventory(recipes, normalizeAccents) {
             .flat(1));
     }else{
         return new Set (recipes.map(recipe => recipe.ustensils
-            .map(u => u.toLocaleLowerCase().normalize('NFD')))
+            .map(u => u))
             .flat(1));
     }
 
@@ -264,7 +264,7 @@ function clickInventoryItem(e) {
     const theme = parentBtn.dataset.theme;
     const inventoryType = parentBtn.dataset.inventory;
     const alertContainer = document.querySelector('.js-tag-container');
-    addKeyword(item.normalize('NFD').replace(/\p{Diacritic}/gu, ''),inventoryType);
+    addKeyword(item.toLocaleLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, ''),inventoryType);
     addAlertTag(item,theme,inventoryType,alertContainer);
     const filteredRecipes = searchKeywords(recipes);
     updateDisplayedRecipes(filteredRecipes);
@@ -284,7 +284,7 @@ function addAlertTag(item,theme,inventoryType,alertContainer,
 }
 
 function removeAlertTag(e) {
-    const item = e.target.previousSibling.previousSibling.textContent.normalize('NFD').replace(/\p{Diacritic}/gu, '');
+    const item = e.target.previousSibling.previousSibling.textContent.toLocaleLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, '');
     const inventoryType = e.target.parentElement.dataset.inventory;
 
     removeKeyword(item,inventoryType);
